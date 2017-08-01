49b51688f9a440f04f50fbf8a34fa6db5ebabcec
Joe Calvillo <jcalvillo757@gmail.com>
2017-08-01 15:46:49 -0600

    Dynamic flexing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
 import { Meta, Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(meta: Meta, title: Title) {
+  gridFlex: number;
+  fxFlex: number;
+  width: number;
+  height: number;
+  orientation: string;
+
+  constructor(meta: Meta, title: Title, private change: ChangeDetectorRef) {
     title.setTitle('Joe Calvillo Portfolio');
 
     meta.addTags([
       { name: 'keywords', content: 'Joe Calvillo Portfolio'},
       { name: 'description', content: 'Joe Calvillo Portfolio'}
     ]);
+  }
+  ngOnInit(event) {
+    this.change.markForCheck();
+    this.width = window.innerWidth || ~~event.target.innerWidth;
+    this.height = window.innerHeight || ~~event.target.innerHeight;
+    this.orientation = this.width > this.height ? "landscape" : "portrait";
+    this.gridFlex = ~~(this.height / 2);
+    this.fxFlex = this.orientation === "portrait" && this.height < 1000  ? ~~((this.width / this.height) * 140) : (this.orientation === "portrait" && this.height > 1000 ? ~~((this.width / this.height) * 90) : ~~((this.height / this.width) * 75));
   }
 }
