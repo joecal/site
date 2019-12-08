0a03ab96ac78f8d74b7e7c88fac3185b4a375d3e
Development <jcalvillo757@gmail.com>
2019-12-08 12:48:40 -0800

    Cleaned up directory

---
+++ src/app/app.component.ts
@@ -0,0 +1,172 @@
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  Inject,
+  PLATFORM_ID,
+  HostListener,
+  ViewChildren,
+  ElementRef,
+  QueryList
+} from "@angular/core";
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from "@angular/animations";
+import { Meta, Title } from "@angular/platform-browser";
+import { isPlatformBrowser } from "@angular/common";
+import * as Trianglify from "trianglify";
+
+@Component({
+  selector: "app-root",
+  animations: [
+    trigger("opacity", [
+      state("show", style({ opacity: 1 })),
+      transition("* => *", animate("2s ease-in-out"))
+    ])
+  ],
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
+})
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChildren("app,home,about,skills,work,contact") children: QueryList<
+    ElementRef
+  >;
+  opacity: string;
+  homeOpacity: number;
+  aboutOpacity: number;
+  skillsOpacity: number;
+  workOpacity: number;
+  contactOpacity: number;
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: object,
+    meta: Meta,
+    title: Title
+  ) {
+    title.setTitle("Joe Calvillo Portfolio");
+
+    meta.addTags([
+      {
+        name: "keywords",
+        content:
+          "Joe Calvillo Portfolio, Software Engineer, Software Developer, Fullstack Engineer, Fullstack Developer, Web Developer, Web Engineer"
+      },
+      { name: "description", content: "Joe Calvillo Portfolio" }
+    ]);
+  }
+
+  ngOnInit() {
+    // this.onWindowResize(); // TODO
+  }
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const childrenList = this.children.toArray();
+      const appElement = childrenList[0].nativeElement;
+      const pattern = Trianglify({
+        width: appElement.clientWidth,
+        height: appElement.clientHeight,
+        variance: "1",
+        cell_size: 200,
+        x_colors: [
+          "#FFFFFF",
+          "#fafafa",
+          "#e0e0e0",
+          "#fafafa",
+          "#e0e0e0",
+          "#fafafa",
+          "#e0e0e0",
+          "#fafafa",
+          "#FFFFFF"
+        ],
+        seed: "muzbg",
+        y_colors: "match_x",
+        color_space: "rgb"
+      });
+
+      document.body.style["background-image"] = "url(" + pattern.png() + ")";
+
+      setTimeout(() => {
+        this.opacity = "show";
+      }, 0);
+    }
+  }
+
+  @HostListener("window:resize", [])
+  onWindowResize() {
+    if (isPlatformBrowser(this.platformId)) {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      const orientation = width > height ? "landscape" : "portrait";
+    }
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.homeOpacity = this.setOpacity(null, "home");
+      this.aboutOpacity = this.setOpacity("skills", "about");
+      this.skillsOpacity = this.setOpacity("work", "skills");
+      this.workOpacity = this.setOpacity("contact", "work");
+      this.contactOpacity = this.setOpacity(null, "contact");
+    }
+  }
+
+  private getElements() {
+    const childrenList = this.children.toArray();
+    const homeElement = childrenList[1].nativeElement;
+    const aboutElement = childrenList[2].nativeElement;
+    const skillsElement = childrenList[3].nativeElement;
+    const workElement = childrenList[4].nativeElement;
+    const contactElement = childrenList[5].nativeElement;
+
+    return {
+      home: homeElement,
+      about: aboutElement,
+      skills: skillsElement,
+      work: workElement,
+      contact: contactElement
+    };
+  }
+
+  private inView(element: HTMLElement) {
+    if (isPlatformBrowser(this.platformId)) {
+      const top = element.offsetTop;
+      return top < window.pageYOffset + window.innerHeight;
+    }
+  }
+
+  private setOpacity(elementName1: string, elementName2: string) {
+    if (isPlatformBrowser(this.platformId)) {
+      let height = window.innerHeight;
+      const width = window.innerWidth;
+      const scrollTop =
+        window.pageYOffset !== undefined
+          ? window.pageYOffset
+          : document.documentElement.scrollTop || document.body.scrollTop;
+      height = height / 3;
+      const value =
+        elementName2 === "home"
+          ? (height - scrollTop) / height
+          : elementName2 === "contact"
+          ? -(height - (scrollTop - this.getElements().contact.offsetTop)) /
+              height +
+            3
+          : this.inView(this.getElements()[elementName1])
+          ? (height -
+              (scrollTop - this.getElements()[elementName2].offsetTop)) /
+            height
+          : -(
+              height -
+              (scrollTop - this.getElements()[elementName2].offsetTop)
+            ) /
+              height +
+            3;
+      return value;
+    }
+  }
+}
